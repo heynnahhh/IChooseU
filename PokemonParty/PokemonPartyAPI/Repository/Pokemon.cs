751e33c8ff4c0606b004c23136c877992bfe0f39
Hannah Liao <hannahpatricia.liao@gmail.com>
2022-02-21 12:42:31 +0800

    Uploading PokemonPartyAPI Project files

---
+++ PokemonParty/PokemonPartyAPI/Repository/Pokemon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json.Linq;
+
+namespace PokeAPI.Net.Repository
+{
+    public class Types
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("pokemon")]
+        public List<TypePokemon> Pokemon { get; set; }
+    }
+
+    public class TypePokemon
+    {
+        [JsonPropertyName("slot")]
+        public int Slot { get; set; }
+
+        [JsonPropertyName("pokemon")]
+        public JObject Pokemon { get; set; }
+    }
+
+}
