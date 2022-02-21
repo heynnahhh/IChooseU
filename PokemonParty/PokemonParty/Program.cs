88b7a6c7cf780d5d2c059a3d90d92ef484633eff
Hannah Liao <hannahpatricia.liao@gmail.com>
2022-02-21 12:36:37 +0800

    Uploading files for PokemonParty Project

---
+++ PokemonParty/PokemonParty/Program.cs
@@ -0,0 +1,108 @@
+﻿using PokemonParty.Net.Utilities;
+using PokeAPI.Net;
+using System.Text.RegularExpressions;
+
+namespace PokemonParty.Net
+{
+    class Program
+    {
+        /// <summary>
+        /// Entrypoint of the console app
+        /// </summary>
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Welcome to IChooseU: A Pokemon Team Generator! \r\n" +
+                "This app shall give you a Team of 5 random Pokemons depending on the type you want to choose.");
+
+            bool shouldCreateTeam = false;
+
+            do
+            {
+                string message = "\r\nAre you ready to choose your Pokemons? [y/n] ";
+
+                bool isReady = Utils.Confirmation(message);
+                string generatedTeam = String.Empty;
+
+
+                if (isReady)
+                {
+                    Console.Clear();
+                    generatedTeam = await OnStart();
+
+                    if (generatedTeam != string.Empty)
+                    {
+                        Console.WriteLine("\r\nCongrats you have created your Pokemon Team! \r\n" +
+                        "Team Members: {0}", generatedTeam);
+                    }
+                    else
+                    {
+                        Console.WriteLine("\r\nThere was a problem generating your team. Please try again..");
+                    }
+
+                    bool isHappy = Utils.Confirmation("\r\nAre you satisfied with your team? [y/n] ");
+
+                    if (!isHappy)
+                    {
+                        shouldCreateTeam = Utils.Confirmation("\r\nDo you wish to generate another team? [y/n] ");
+                    }
+                }
+                else
+                {
+                    shouldCreateTeam = false;
+                }
+
+            }
+            while (shouldCreateTeam);
+
+            Console.WriteLine("\r\nThank you for using IChooseU. Have a nice day!");
+
+        }
+
+        /// <summary>
+        /// Initialization of Inputs
+        /// </summary>
+        private static async Task<string> OnStart()
+        {
+            Console.WriteLine("There are currently 18 types of Pokemon. \r\n" +
+                "Normal, Fire, Fighting, Water, Flying, Grass, Poison, Electric, Ground \r\n" +
+                "Psychic, Rock, Ice, Bug, Dragon, Ghost, Dark, Steel and Fairy...");
+            
+            await Task.Delay(3000);
+            Console.WriteLine("\r\nOut of these, You can pick 1 up to 5 types to assemble your team.");
+            
+            await Task.Delay(1000);
+            string message = "\r\nInput how many types you wish to choose [1-5]: ";
+
+            int typeCount = Utils.ProcessIntInput(message);
+            string pokemonTeam = String.Empty;
+
+            try
+            {
+                int counter = 1;
+                List<string> typeList = new List<string>();
+
+                while (counter <= typeCount)
+                {
+                    Console.Write("\r\nInput Pokemon type {0}: ", counter);
+                    typeList.Add(Console.ReadLine()); //input validation here
+                    counter++;
+                }
+
+                if (typeList.Count > 0)
+                {
+                    Console.WriteLine("\r\nPlease wait while I generate your team...");
+                    pokemonTeam = await PokeAPIWebRequest.GenerateTeamAsync(typeList);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return pokemonTeam;
+
+        } 
+        
+    }
+}
---
+++ PokemonParty/PokemonParty/Utilities.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace PokemonParty.Net.Utilities
+{
+    /// <summary>
+    /// Utility Methods that can be imported as needed
+    /// </summary>
+    public class Utils
+    {
+
+        public static void ShowMessage(string message)
+        {
+            Console.Write(message);
+        }
+
+        #region Input Validations
+        public static int ProcessIntInput(string message)
+        {
+            //bool isValid = false;
+            ConsoleKeyInfo inputKeyInfo;
+            int doCounter = 0;
+            int parsedKey = 0;
+
+            do
+            {
+                if (doCounter > 0)
+                {
+                    message = "\r\nPlease choose from 1 - 5 only: ";
+                }
+
+                ShowMessage(message);
+                inputKeyInfo = Console.ReadKey(false);
+
+                if (Char.IsDigit(inputKeyInfo.KeyChar))
+                {
+                    parsedKey = Convert.ToInt32(inputKeyInfo.KeyChar.ToString());
+                }
+
+                doCounter++;
+
+            }
+            while (parsedKey > 5 || parsedKey < 1);
+
+            return parsedKey;
+        }
+
+
+        public static bool Confirmation(string message)
+        {
+            bool isConfirmed = false;
+            ConsoleKey response;
+
+            do
+            {
+                ShowMessage(message);
+                response = Console.ReadKey(false).Key;
+            }
+            while (response != ConsoleKey.Y && response != ConsoleKey.N);
+
+            isConfirmed = (response == ConsoleKey.Y);
+            return isConfirmed;
+        }
+
+        public static bool RegexValidation(string input)
+        {
+            Regex regexValidation = new Regex(@"^(dark)$");
+            return regexValidation.IsMatch(input);
+        }
+
+        #endregion
+    }
+}
